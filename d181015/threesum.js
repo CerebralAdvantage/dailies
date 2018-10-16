6fc80b015a540c2bca09b231c95590a7a8b8428e
Jim Huddle <james.r.huddle@gmail.com>
2018-10-15 20:15:46 -0400

    genned array and target

--- d181015/threesum.js
+++ d181015/threesum.js
@@ -1,5 +1,17 @@
 const arraySize = 10000;
 const minval = 0;
-const minval =    1000000;
+const maxval =    1000000;
 const mintarget =  500000;
 const maxtarget = 1500000;
+let arr = [];
+
+function init()
+{
+  let i=0;
+  for(i=0;i<arraySize;i++) arr[i] = Math.trunc(Math.random()*(maxval-minval))+minval;
+  let target = Math.trunc(Math.random()*(maxtarget-mintarget))+mintarget;
+  //for(i=0;i<arraySize;i++) console.log(arr[i]);
+  //console.log("HEY!");
+}
+
+init();
